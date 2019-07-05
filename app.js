bd0fd1940e6504b95f98787a498182a510b8a68f
Chiphyr <alexsssthereal@yahoo.com>
2019-07-05 13:15:11 +0100

    Add more errors

--- app.js
+++ app.js
@@ -28,129 +28,226 @@ app.get('/', (req, res) => {
 });
 
 app.post('/restcord/channels/:id/send', (req, res) => {
-    if(!req.body.content){
-        err('Send: No content.');
-        return res.status(400).send({
+    try{
+        if(!req.body.content){
+            err('Send: 400 Bad Request');
+            return res.status(400).send({
+                success: false,
+                message: "Required: content."
+            });
+        };
+        let channel = client.channels.get(`${req.params.id}`);
+        let m = channel.send(`${req.body.content}`);
+        suc('Message: 201 Created');
+        return res.status(201).send({
+            success: true,
+            message: 'The message has been sent.'
+        });
+    } catch (e) {
+        err('Send: 500 Internal Server Error');
+        return res.status(500).send({
             success: false,
-            message: "Required: content."
+            message: 'The message could not be sent.'
         });
     };
-    let channel = client.channels.get(`${req.params.id}`);
-    channel.send(`${req.body.content}`);
-    suc('Message sent.');
-    return res.status(201).send({
-        success: true,
-        message: 'The message has been sent.'
-    });
 });
 
 app.post('/restcord/users/:id/send', (req, res) => {
-    if(!req.body.content){
-        err("DM: No content.");
-        return res.status(400).send({
-            success: false,
-            message: 'Required: content.'
+    try{
+        if(!req.body.content){
+            err("DM: 400 Bad Request");
+            return res.status(400).send({
+                success: false,
+                message: 'Required: content.'
+            });
+        };
+        let user = client.users.get(`${req.params.id}`);
+        let m = user.send(`${req.body.content}`);
+        suc('DM: 201 Created');
+        return res.status(201).send({
+            success: true,
+            message: 'The DM has been sent.'
         });
-    };
-    let user = client.users.get(`${req.params.id}`);
-    user.send(`${req.body.content}`);
-    suc('DM sent.');
-    return res.status(201).send({
-        success: true,
-        message: 'The DM has been sent.'
-    });
+    } catch(e){
+        err('DM: 500 Internal Server Error');
+        return res.status(500).send({
+            success: false,
+            message: 'The DM could not be sent.'
+        });
+    }
 });
 
 app.get('/restcord/guilds', (req, res) => {
-    let guilds = client.guilds;
-    suc('Guilds given.');
-    return res.status(200).send({
-        success: true,
-        message: `I gathered the guilds successfully.`,
-        guilds
-    });
+    try{
+        let guilds = client.guilds;
+        suc('Guilds: 200 OK');
+        return res.status(200).send({
+            success: true,
+            message: `I gathered the guilds successfully.`,
+            guilds
+        });
+    } catch(e){
+        err('Guilds: 500 Internal Server Error');
+        return res.status(500).send({
+            success: true,
+            message: 'I could not gather the guilds.'
+        });
+    }
 });
 
 app.get('/restcord/channels', (req, res) => {
-    let channels = client.channels;
-    suc('Channels given.');
-    return res.status(200).send({
-        success: true,
-        message: 'I gathered all the channels successfully.',
-        channels
-    });
+    try {
+        let channels = client.channels;
+        suc('Channels: 200 OK');
+        return res.status(200).send({
+            success: true,
+            message: 'I gathered all the channels successfully.',
+            channels
+        });
+    } catch (e) {
+        err('Channels: 500 Internal Server Error');
+        return res.status(500).send({
+            success: false,
+            message: 'I could not gather the channels.'
+        });
+    }
 });
 
 app.get('/restcord/users', (req, res) => {
-    let users = client.users;
-    suc('Users given.');
-    return res.status(200).send({
-        success: true,
-        message: 'I gathered all the users successfully.',
-        users
-    });
+    try {
+        let users = client.users;
+        suc('Users: 200 OK');
+        return res.status(200).send({
+            success: true,
+            message: 'I gathered all the users successfully.',
+            users
+        });
+    } catch (e) {
+        err('Users: 500 Internal Server Error');
+        return res.status(500).send({
+            success: false,
+            message: "I could not gather the users."
+        });
+    }
 });
 
 app.post('/restcord/guilds/:guildid/users/:userid/ban', (req, res) => {
-    let guild = client.guilds.get(`${req.params.guildid}`);
-    let member = guild.members.get(`${req.params.userid}`);
-    member.ban();
-    suc('Banned a user.');
-    return res.status(200).send({
-        success: true,
-        message: 'I banned that member.'
-    });
+    try {
+        let guild = client.guilds.get(`${req.params.guildid}`);
+        let member = guild.members.get(`${req.params.userid}`);
+        member.ban();
+        suc('Ban: 200 OK');
+        return res.status(200).send({
+            success: true,
+            message: 'I banned that member.'
+        });
+    } catch (e) {
+        err('Ban: 500 Internal Server Error');
+        return res.status(500).send({
+            success: false,
+            message: 'I could not ban that member.'
+        });
+    }
 });
 
 app.post('/restcord/guilds/:guildid/users/:userid/kick', (req, res) => {
-    let guild = client.guilds.get(`${req.params.guildid}`);
-    let member = guild.members.get(`${req.params.userid}`);
-    member.kick();
-    suc('Kicked a user.');
-    return res.status(200).send({
-        success: true,
-        message: 'I kicked that member.'
-    });
+    try {
+        let guild = client.guilds.get(`${req.params.guildid}`);
+        let member = guild.members.get(`${req.params.userid}`);
+        member.kick();
+        suc('Kick: 200 OK');
+        return res.status(200).send({
+            success: true,
+            message: 'I kicked that member.'
+        });
+    } catch (e) {
+        err('Kick: 500 Internal Server Error');
+        return res.status(500).send({
+            success: false,
+            message: 'I could not kick that member.'
+        });
+    }
 });
 
 app.get('/restcord/users/:id', (req, res) => {
-    let user = client.users.get(`${req.params.id}`);
-    suc('User given.');
-    return res.status(200).send({
-        success: true,
-        message: 'I gathered that user.',
-        user
-    });
+    try {
+        let user = client.users.get(`${req.params.id}`);
+        suc('User: 200 OK');
+        return res.status(200).send({
+            success: true,
+            message: 'I gathered that user.',
+            user
+        });
+    } catch (e) {
+        err('User: 500 Internal Server Error');
+        return res.status(500).send({
+            success: false,
+            message: 'I could not get that user.'
+        });
+    }
 });
 
 app.get(`/restcord/channels/:id`, (req, res) => {
-    let channel = client.channels.get(`${req.params.id}`);
-    suc('Channel given.');
-    return res.status(200).send({
-        success: true,
-        message: 'I gathered that channel.',
-        channel
-    });
+    try {
+        let channel = client.channels.get(`${req.params.id}`);
+        suc('Channel: 200 OK');
+        return res.status(200).send({
+            success: true,
+            message: 'I gathered that channel.',
+            channel
+        });
+    } catch (e) {
+        err('Channel: 500 Internal Server Error');
+        return res.status(500).send({
+            success: false,
+            message: 'I could not get that channel.'
+        });
+    }
 });
 
 app.get('/restcord/guilds/:id', (req, res) => {
-    let guild = client.guilds.get(`${req.params.id}`);
-    suc('Guild given.');
-    return res.status(200).send({
-        success: true,
-        message: "I gathered that guild.",
-        guild
-    });
+    try {
+        let guild = client.guilds.get(`${req.params.id}`);
+        suc('Guild: 200 OK');
+        return res.status(200).send({
+            success: true,
+            message: "I gathered that guild.",
+            guild
+        });
+    } catch (e) {
+        err('Guild: 500 Internal Server Error');
+        return res.status(500).send({
+            success: true,
+            message: 'I could not gather that guild.'
+        });
+    }
 });
 
 app.delete('/restcord/channels/:id', (req, res) => {
-    let channel = client.channels.get(`${req.params.id}`);
-    channel.delete();
-    suc('Deleted a channel.');
-    return res.status(200).send({
-        success: true,
-        message: 'I deleted the channel.'
+    try {
+        let channel = client.channels.get(`${req.params.id}`);
+        channel.delete();
+        suc('Channel Delete: 200 OK');
+        return res.status(200).send({
+            success: true,
+            message: 'I deleted the channel.'
+        });
+    } catch (e) {
+        err('Channel Delete: 500 Internal Server Error');
+        return res.status(500).send({
+            success: false,
+            message: 'I could not get that channel.'
+        });
+    }
+});
+
+app.get('*', (req, res) => {
+    err('404 Not Found');
+    res.status(404).send({
+        success: false,
+        message: 'We could not find what you were looking for...'
     });
+    return res.render('404');
 });
 
 app.listen(config.port, () => {
